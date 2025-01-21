5c13501b360e6b725e2d54e164470836e26fcf63
panha <hkmkh26@gmail.com>
2025-01-20 23:31:23 -0800

    structure

---
+++ structure/structure_2.c
@@ -0,0 +1,29 @@
+#include<stdio.h>
+// structure structure_name
+struct Employee{
+    int id;
+    char name[100];
+    double salary;
+    char gender[10];
+}employee;// global object structure
+void IO()
+{
+    printf("Employee code : ");scanf("%d",&employee.id);
+    printf("Employee name : ");scanf("%s",employee.name);
+    printf("Employee salary : ");scanf("%lf",&employee.salary);
+    printf("Employee gender : ");scanf("%s",employee.gender);
+
+    printf("\nEmployee Details:\n");
+    printf("Employee Code : %d\n",employee.id);
+    printf("Employee Name : %s\n",employee.name);
+    printf("Employee Salary : %.2lf\n",employee.salary);
+    printf("Employee Gender : %s\n",employee.gender);
+}
+int main()
+{
+    // structure / object structure
+    // local
+    // struct Employee employee;
+    IO();
+    return 0;
+}
